3284893f62f98b997f16acbe163f0020faea76a7
Suleman Saiyed <sulemansaiyed20@gmail.com>
2025-08-01 14:11:37 +0530

    Add Flavours section with images

--- milkshake-main/src/App.jsx
+++ milkshake-main/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 
 import Hero from './components/Hero';
+import Flavours from './components/Flavours';
 
 function App() {
   return (
     <>
       <Navbar />
  <Hero/>
+ <Flavours/>
     </>
   );
 }
