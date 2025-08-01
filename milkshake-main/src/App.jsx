17558fe90ca1cfbbe42b7f6b09a72f21113fb959
Suleman Saiyed <sulemansaiyed20@gmail.com>
2025-08-01 14:29:45 +0530

    adding footer section

--- milkshake-main/src/App.jsx
+++ milkshake-main/src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar';
 
 import Hero from './components/Hero';
 import Flavours from './components/Flavours';
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Navbar />
  <Hero/>
  <Flavours/>
+ <About/>
+ <Footer/>
     </>
   );
 }
