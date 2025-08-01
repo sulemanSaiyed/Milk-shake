e66cbe3f9b0f3845d227de8630228c4030570c77
Suleman Saiyed <sulemansaiyed20@gmail.com>
2025-08-01 14:05:13 +0530

    Add Hero section

--- milkshake-main/src/App.jsx
+++ milkshake-main/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 
-
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1>Hello Vite + React</h1>
+ <Hero/>
     </>
   );
 }
---
+++ milkshake-main/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <section className="hero" id="hero">
+      <div className="hero-content">
+        <h2>Welcome to Our MilkShake Shop</h2>
+        <p>Delicious flavours made with love 🥤</p>
+        <button>Order Now</button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
