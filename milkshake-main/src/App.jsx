0ac57c8ab60e415895868eec2184b49633c67842
pandu-12-2000 <crackivar@gmail.com>
2025-08-14 00:14:27 +0530

    removeing global styles and applying to each comeonets

--- milkshake-main/src/App.jsx
+++ milkshake-main/src/App.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-
 import Hero from './components/Hero';
-import Flavours from './components/Flavours';
-import About from './components/About';
-import Footer from './components/Footer';
+
 
 function App() {
   return (
     <>
-      <Navbar />
+   
  <Hero/>
- <Flavours/>
- <About/>
- <Footer/>
+
     </>
   );
 }
